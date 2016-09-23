44609970118a0a965df19f8687d257fe22d6c1e0
timnettest1 <timnettest1@gmail.com>
2016-09-22 23:00:05 -0700

    Clear

--- Learn/Learn/Program.cs
+++ Learn/Learn/Program.cs
@@ -11,37 +11,5 @@ namespace Learn
         static void Main(string[] args)
         {
         }
-
-        /// <summary>
-        /// 2
-        /// 3
-        /// 7
-        /// </summary>
-        private void master()
-        {
-        }
-
-        /// <summary>
-        /// 1
-        /// 6
-        /// 8
-        /// </summary>
-        private void dev()
-        {
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        private void fa()
-        {
-        }
-
-        /// <summary>
-        /// 5 
-        /// </summary>
-        private void fb()
-        {
-        }
     }
 }
